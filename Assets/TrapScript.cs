af5f58aa24b1aba311604e80bcf4d07e0ad6814d
InanePotato <michaelburke@mmc.school.nz>
2023-08-01 21:35:18 +1200

    Added a spike trap
    Creates and made a prefab for a spike trap. This included making a trap script that can be used to control it, as well as some animations for the trap.

---
+++ Assets/TrapScript.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapScript : MonoBehaviour
+{
+    private GameManagerScript gameManagerScript;
+
+    public bool active;
+    public bool canAttackPlayer;
+    public float delayTime;
+    public float damagePerSecond;
+    [SerializeField]
+    private float delayTimeCounter;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerScript = GameManagerScript.Instance;
+
+        animator = gameObject.GetComponent<Animator>();
+
+        canAttackPlayer = false;
+        active = false;
+        delayTimeCounter = delayTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManagerScript.gamePaused || gameManagerScript.gameOver)
+        {
+            return;
+        }
+
+        if (delayTimeCounter <= 0)
+        {
+            delayTimeCounter = delayTime;
+            active = !active;
+            
+            if (active)
+            {
+                animator.Play("activateSpikes");
+            }
+            else
+            {
+                animator.Play("deactivateSpikes");
+            }
+        }
+        else
+        {
+            delayTimeCounter -= Time.deltaTime;
+        }
+
+        if (active && canAttackPlayer)
+        {
+            float damage = Time.deltaTime * damagePerSecond;
+            gameManagerScript.player.GetComponent<PlayerStats>().SubtractHealth(damage);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canAttackPlayer = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canAttackPlayer = false;
+    }
+}
