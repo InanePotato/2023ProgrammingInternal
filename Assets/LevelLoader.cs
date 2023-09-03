74f2553914aecfab6dc259c0aeba13fce8d223a6
InanePotato <michaelburke@mmc.school.nz>
2023-09-03 23:27:43 +1200

    Added scene transition and animation
    Note: current bugs
    - Other canvases cover up the transition canvas.
    - Loading animation doesn't play properly.

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1.334f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNewScene(int buildIndex)
+    {
+        StartCoroutine(LoadScene(buildIndex));
+    }
+
+    IEnumerator LoadScene(int buildIndex)
+    {
+        // Play Start animation
+        transition.SetTrigger("Start");
+
+        // Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        // Start Loading Scene
+        AsyncOperation operation = SceneManager.LoadSceneAsync(buildIndex);
+
+        // Play loading animation
+        transition.SetTrigger("Loading");
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            Debug.Log(progress);
+
+            yield return null;
+        }
+    }
+}
