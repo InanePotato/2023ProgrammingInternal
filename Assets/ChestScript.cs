3cb0bd598010ef6eae0c0091c0d08267417be996
InanePotato <michaelburke@mmc.school.nz>
2023-09-03 12:14:57 +1200

    Added loot tables to chests
    Includes some efficiency increases.

--- Assets/ChestScript.cs
+++ Assets/ChestScript.cs
@@ -22,14 +22,15 @@ public class ChestScript : MonoBehaviour
     private Transform chestTransform;
     public float lootSpawnOffsetY;
     public float lootSpawnOffsetX;
-    const float LOOT_SPAWN_DISTANCE_BELOW = 0.2f;
+    public float lootSpawnDistanceBelow = 0.2f;
     public Sprite openSprite;
     public Sprite closedSprite;
 
     [Header("Required Loot")]
     public List<LootItem> loot = new List<LootItem>();
-    //public List<Item> requiredLootItemObjects = new List<Item>();
-    //public List<int> requiredLootItemAmmounts = new List<int>();
+    public LootTable lootTable;
+    public int minLootTableDrops;
+    public int maxLootTableDrops;
 
     [Serializable]
     public struct LootItem
@@ -78,22 +79,56 @@ public class ChestScript : MonoBehaviour
             // release/spawn in loot
             foreach (LootItem lootItem in loot)
             {
-                GameObject newItemDrop = Instantiate(droppedItem);
-
-                float X = chestTransform.position.x + UnityEngine.Random.Range(-lootSpawnOffsetX, lootSpawnOffsetX);
-                float Y = chestTransform.position.y + UnityEngine.Random.Range(-lootSpawnOffsetY, lootSpawnOffsetY) - LOOT_SPAWN_DISTANCE_BELOW;
-                newItemDrop.transform.position = new Vector2(X, Y);
-
-                newItemDrop.transform.Rotate(Vector3.forward, UnityEngine.Random.Range(-80, 80));
-
-                newItemDrop.GetComponent<ItemPickup>().item = lootItem.item;
                 int ammount = UnityEngine.Random.Range(lootItem.minAmmount, lootItem.maxAmmount);
-                newItemDrop.GetComponent<ItemPickup>().ammount = ammount;
+                DropItem(lootItem.item, ammount);
+            }
+
+            // release loot table loot
+            if (lootTable != null && maxLootTableDrops > 0)
+            {
+                // prepare table for item selection based on 'rarity'
+                List<int> LootTableItemIDS = new List<int>();
+                for (int id = 0; id < lootTable.lootTable.Count(); id++)
+                {
+                    for (int i = 0; i < lootTable.lootTable[id].rarity; i++)
+                    {
+                        LootTableItemIDS.Add(id);
+                    }
+                }
+
+                // drop loot table loot
+                int randomNumDrops = UnityEngine.Random.Range(minLootTableDrops, maxLootTableDrops);
+                for (int i = 0; i < randomNumDrops; i++)
+                {
+                    //get a random id to drop
+                    int randomID = LootTableItemIDS[UnityEngine.Random.Range(0, LootTableItemIDS.Count() - 1)];
+
+                    // get ammount
+                    int ammount = UnityEngine.Random.Range(lootTable.lootTable[randomID].minAmmountPerDrop, lootTable.lootTable[randomID].maxAmmountPerDrop);
+                    // get Item
+                    Item item = lootTable.lootTable[randomID].item;
+
+                    DropItem(item, ammount);
+                }
             }
         }
         else
         {
             chestSpriteRenderer.sprite = closedSprite;
         }
+    }
+
+    private void DropItem(Item item, int ammount)
+    {
+        GameObject newItemDrop = Instantiate(droppedItem);
+
+        float X = chestTransform.position.x + UnityEngine.Random.Range(-lootSpawnOffsetX, lootSpawnOffsetX);
+        float Y = chestTransform.position.y + UnityEngine.Random.Range(-lootSpawnOffsetY, lootSpawnOffsetY) - lootSpawnDistanceBelow;
+        newItemDrop.transform.position = new Vector2(X, Y);
+
+        newItemDrop.transform.Rotate(Vector3.forward, UnityEngine.Random.Range(-80, 80));
+
+        newItemDrop.GetComponent<ItemPickup>().item = item;
+        newItemDrop.GetComponent<ItemPickup>().ammount = ammount;
     }
 }
